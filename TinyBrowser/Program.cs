fbfe13745a9b9f792a073744bf6bf64af9ef21bb
Oskar Jansson <oskar_slim89@hotmail.com>
2021-04-30 11:34:58 +0200

    implemented a FindAll method. But it simply prints to console. So its not very useful

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -26,9 +27,12 @@ namespace TinyBrowser
             stream.Read(respons);
             var responsString = Encoding.ASCII.GetString(respons);
 
+            //Console.WriteLine(responsString);
             Console.WriteLine(getBetween(responsString, "<title>", "</title>"));
             
-
+            FindAllBetween(responsString, "<a href=\"", "\"");
+            
+            
             Console.ReadLine();
         }
 
@@ -43,6 +47,26 @@ namespace TinyBrowser
             }
 
             return "";
+        }
+        
+        public static void FindAllBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int Start, End = 0, lastEnd = 0;
+                while (true)
+                {
+            
+                    Start = strSource.IndexOf(strStart, End) + strStart.Length;
+                    End = strSource.IndexOf(strEnd, Start);
+                    if (End < lastEnd) break;
+                    lastEnd = End;
+                    
+                    
+                    Console.WriteLine(strSource.Substring(Start, End - Start));
+
+                }
+            }
         }
         
     }
